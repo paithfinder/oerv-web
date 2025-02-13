196af89a5db5e944bb90321a818876e42847db3e
Mingle Tan <mingle.oerv@isrc.iscas.ac.cn>
2025-02-13 16:27:13 +0800

    代码框字体颜色调整
    修改highlight.js配置
    
    Signed-off-by: Mingle Tan <mingle.oerv@isrc.iscas.ac.cn>

--- src/components/SystemSelect.vue
+++ src/components/SystemSelect.vue
@@ -3,13 +3,14 @@ import "github-markdown-css";
 import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
 import hljs from "highlight.js";
-import "highlight.js/styles/atom-one-dark.css";
+import "highlight.js/styles/github.css";
 const marked = new Marked(
   markedHighlight({
     langPrefix: "hljs language-",
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : "shell";
-      return hljs.highlight(code, { language }).value;
+    highlight(code) {
+      return hljs.highlightAuto(code).value;
+      // const language = "plaintext";
+      // return hljs.highlight(code, { language }).value;
     }
   })
 );
